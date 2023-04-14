eb08954db613558d8e72d0e51865a53bad688127
KHoaDangggg <khoa.dp210486@sis.hust.edu.vn>
2023-04-15 02:28:16 +0700

    Second commit

--- lab1/FirstDialog.java
+++ lab1/FirstDialog.java
@@ -1,5 +1,5 @@
 // Example 2: FirstDialog.java
-import javax.swing,JOptionPane
+import javax.swing.JOptionPane;
 public class FirstDialog{
 	public static void main(String args[]){
 		JOptionPane.showMessageDialog(null,"Hello, world! How are you?");
--- lab1/mathOperations.java
+++ lab1/mathOperations.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 public class mathOperations {
     public static void main(String args[]){
         Scanner keyboard = new Scanner(System.in);
-        Double num1 = keyboard.nextDouble();
-        Double num2 = keyboard.nextDouble();
+        Double num1 = Double.parseDouble(keyboard.nextLine());
+        Double num2 = Double.parseDouble(keyboard.nextLine());
         System.out.print("Operation: ");
         String operation = keyboard.nextLine();
         if(operation.equals("+")){
-            System.out.println("Sum: " +  (num2 + num1));
+            System.out.printf("Sum: %.2f ", (num2 + num1));
         }
         if(operation.equals("-")){
-            System.out.println("Difference: " +  (num2 - num1));
+            System.out.printf("Difference: %.2f" , (num2 - num1));
         }
         if(operation.equals("*")){
-            System.out.println("Product: " +  (num2 * num1));
+            System.out.printf("Product: %.2f" , (num2 * num1));
         }
         if(operation.equals("/")){
             if(num1 == 0){
                 System.out.println("Invalid divisor");
             }
             else{
-                System.out.println("Division: " +  (num2/num1));
+                System.out.printf("Division: %.2f", num2/num1);
             }
         }
     }
