d27dae7cb1bf472bb6621a1ff0178e44633ba13c
KHoaDangggg <khoa.dp210486@sis.hust.edu.vn>
2023-05-14 16:14:33 +0700

    Fix doc

--- Lab03/OtherProjects/src/hust/soict/ICT/lab01/Others/FirstDialog.java
+++ Lab03/OtherProjects/src/hust/soict/ICT/lab01/Others/FirstDialog.java
@@ -1,6 +1,6 @@
 package hust.soict.ICT.lab01.Others;// Example 2: FirstDialog.java
 import javax.swing.JOptionPane;
-public class FirstDialog{
+public class git FirstDialog{
 	public static void main(String args[]){
 		JOptionPane.showMessageDialog(null,"Hello, world! How are you?");
 		System.exit(0);
